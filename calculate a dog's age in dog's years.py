648c73bc57a114f035f8fd1828efc6ebe1d9794d
dineth-nirmal <55383415+dineth-nirmal@users.noreply.github.com>
2019-09-23 19:30:32 +0530

    this is an exercise from w3resource.com

---
+++ calculate a dog's age in dog's years.py
@@ -0,0 +1,10 @@
+human_years = int(input("your dog's age in human years: "))
+counter = 0
+dog_years = 0
+while counter < human_years:  # if we set this to <= the loop will run extra circle
+	counter += 1
+	if counter <= 2:
+		dog_years += 10.5
+	else:
+		dog_years += 4
+print(f'age in dog years: {dog_years}')
