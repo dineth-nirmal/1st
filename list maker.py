cbb75d3a118e9d29638ef05db29ee77c17f94e16
dineth-nirmal <55383415+dineth-nirmal@users.noreply.github.com>
2019-09-29 11:14:39 +0530

    used functions

--- list maker.py
+++ list maker.py
@@ -1,16 +1,19 @@
-while True:
-    try:
-        list_len = int(input('list length: '))
-        list_1 = []
-        counter = 0
-        while counter < list_len:
-            try:
-                item_input = int(input(f'Number ({counter + 1}): '))
-                list_1.append(item_input)
-                counter += 1
-            except ValueError:
-                print('invalid value')
-        print(list_1)
-        break
-    except ValueError:
-        print('list length should be a numerical value. try again.')
+def list_maker():    
+    while True:
+        try:
+            list_len = int(input('list length: '))
+            list_1 = []
+            counter = 0
+            while counter < list_len:
+                try:
+                    item_input = int(input(f'Number ({counter + 1}): '))
+                    list_1.append(item_input)
+                    counter += 1
+                except ValueError:
+                    print('invalid value')
+            return list_1
+        except ValueError:
+            print('list length should be a numerical value. try again.')
+
+            
+            
