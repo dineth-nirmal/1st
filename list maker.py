5da8217083600fc619e9313725f79fac2966c416
dineth-nirmal <55383415+dineth-nirmal@users.noreply.github.com>
2019-09-29 10:59:56 +0530

    updated list by using loops

--- list maker.py
+++ list maker.py
@@ -1,11 +1,16 @@
-list_lim = int(input('length of list: '))
-list_1 = []
-counter = 0
-while counter < list_lim:
+while True:
     try:
-        number = int(input(f'Number {counter + 1}: '))
-        list_1.append(number)
-        counter += 1
+        list_len = int(input('list length: '))
+        list_1 = []
+        counter = 0
+        while counter < list_len:
+            try:
+                item_input = int(input(f'Number ({counter + 1}): '))
+                list_1.append(item_input)
+                counter += 1
+            except ValueError:
+                print('invalid value')
+        print(list_1)
+        break
     except ValueError:
-        print('list item should be a number')
-print(list_1)
+        print('list length should be a numerical value. try again.')
