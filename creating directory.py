fa41e17da20ad2d34529ca4f94cfb99a22f613ef
dineth-nirmal <55383415+dineth-nirmal@users.noreply.github.com>
2019-10-08 22:50:25 +0530

    updated little bit

--- creating directory.py
+++ creating directory.py
@@ -1,17 +1,24 @@
-def storing(name, age):
-    customers[name] = age
-
-
-customers = {
-
-}
+d_length = int(input('dictionary length: '))
+list_d = {}
 counter = 0
-maximum_limit = 10
-while counter < maximum_limit:
-    name_in = input('Name: ')
-    if name_in == 'print':
-        break
-    age_in = input('age: ')
-    storing(name_in, age_in)
-    counter += 1
-print(customers)
+while counter < d_length:
+    try:
+        key_d = int(input('key: ').replace(' ', ''))
+        value_d = int(input('value: ').replace(' ', ''))
+        if key_d in list_d:
+            print('That key is already in the dictionary')
+            y_or_n = input('Do you want to overwrite it,(yes or no) : ').lower()
+            while True:
+                if y_or_n == 'yes':
+                    list_d[key_d] = value_d
+                    break
+                elif y_or_n == 'no':
+                    break
+                else:
+                    print('invalid value')
+        else:
+            list_d[key_d] = value_d
+            counter += 1
+    except ValueError:
+        print('invalid values')
+print(list_d)
